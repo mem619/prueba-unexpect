ebc707e11253f96ce24a861619d1c3a900317228
Jose G. Rios Tovar <jg.rios@mueblesamerica.mx>
2018-10-29 10:46:19 -0600

    build

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from "vue";
 import VueRouter  from "vue-router";
 import App from "./App.vue";
 import store from "./store";
-import router from "./router";
+//import router from "./router";
 import Vuex from "vuex";
+import hello from "./components/HelloWorld.vue"
 Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
+const router = new VueRouter({
+  mode: 'history',
+  routes: [{ path: '/', component: hello }]
+});
 
 new Vue({
   store,
